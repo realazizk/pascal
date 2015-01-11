abe6a7fd2a22b4727c8d2268f12e3bfc7e3a2ea6
mohamed-aziz <medazizknani@gmail.com>
2015-01-11 12:16:56 +0000

    Game works !

--- tictactoe.pas
+++ tictactoe.pas
@@ -37,7 +37,7 @@ var
   i, choix : integer;
   e   : integer;
   ega, st              : string;
-  a, tourJoueur1, tourJoueur2                : boolean;
+  a, tourJoueur1, tourJoueur2,cho                : boolean;
 
 function compare(valeur1, valeur2, valeur3 : string; var st : string) : boolean;
 begin
@@ -100,6 +100,37 @@ begin
     end;
 end;
 
+procedure check(t : tab;var a: boolean;var scoreJoueur1, scoreJoueur2 : integer);
+var
+  ega, st : string;
+  i   : integer;  
+begin
+  if compare(t[1], t[2], t[3], st) or compare(t[4], t[5], t[6], st) or compare(t[7], t[8], t[9], st)
+   or compare(t[1], t[4], t[7], st) or compare(t[2], t[5], t[8], st) or compare(t[3], t[6], t[9], st)
+   or compare(t[1], t[5], t[9], st) or compare(t[3], t[5], t[7], st) then
+    begin
+      a := false;
+      if st = '1' then
+        begin
+          scoreJoueur1 := scoreJoueur1 + 1;
+          Writeln(nomJoueur1, ' vous avez gagner !'); 
+        end
+       else
+         begin
+           scoreJoueur2 := scoreJoueur2 + 1;
+           Writeln(nomJoueur2, ' vous avez gagner !'); 
+         end;
+   end;
+   ega := '';
+   for i:=1 to 9 do
+     ega := ega + t[i];
+     if (length(ega) = 9 ) and (st = '') then
+       begin
+         a := false;
+         writeln('Egalite !');
+       end;
+end;
+
 begin
   a := true;
   while a do
@@ -110,49 +141,38 @@ begin
       if tourJoueur1 then
         begin
           repeat
-            affichage(t, scoreJoueur1, scoreJoueur2); 
+            cho := false;
             write(nomJoueur1, ' entrer un nombre entre [1, 9] -> ');readln(choix);
-            t[choix] := '1';
+            if t[choix] = '' then
+              begin
+                t[choix] := '1';
+                cho := true;
+              end;
             tourJoueur1 := false;
             tourJoueur2 := true;
-          until choix in [1..9];
-        end;   
-      if tourJoueur2 then
-        begin
-          repeat
-            affichage(t, 0, 0);
-            write(nomJoueur2, ' entrer un nombre entre [1, 9] -> ');readln(choix);
-            t[choix] := '0';
-            tourJoueur2 := false;
-            tourJoueur1 := true;
-          until choix in [1..9];
+          until (choix in [1..9]) and cho;
         end;
-    affichage(t, scoreJoueur1, scoreJoueur2);
-    if compare(t[1], t[2], t[3], st) or compare(t[4], t[5], t[6], st) or compare(t[7], t[8], t[9], st)
-     or compare(t[1], t[4], t[7], st) or compare(t[2], t[5], t[8], st) or compare(t[3], t[6], t[9], st)
-     or compare(t[1], t[5], t[9], st) or compare(t[3], t[5], t[7], st) then
-       begin
-         a := false;
-         if st = '1' then
-           begin
-             scoreJoueur1 := scoreJoueur1 + 1;
-             Writeln(nomJoueur1, ' vous avez gagner !'); 
-           end
-         else
-           begin
-             scoreJoueur2 := scoreJoueur2 + 1;
-             Writeln(nomJoueur2, ' vous avez gagner !'); 
-           end;
-       end;
-
-     ega := '';
-     for i:=1 to 9 do
-       ega := ega + t[i];
-     if length(ega) = 9 then
-       begin
-         a := false;
-         writeln('Egalite !');
-       end;
+      affichage(t, scoreJoueur1, scoreJoueur2);
+      check(t, a, scoreJoueur1, scoreJoueur2);
+      if a then
+        begin
+          if tourJoueur2 then
+            begin
+              repeat
+              cho := false;
+              write(nomJoueur2, ' entrer un nombre entre [1, 9] -> ');readln(choix);
+              if t[choix] = '' then
+                begin
+                  t[choix] := '0';
+                  cho := true;
+                end;
+                tourJoueur2 := false;
+                tourJoueur1 := true;
+              until (choix in [1..9]) and cho;
+            end;
+        affichage(t, scoreJoueur1, scoreJoueur2);
+        check(t, a, scoreJoueur1, scoreJoueur2);
+    end;
 
    end;
 end;
