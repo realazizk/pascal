14278d3e819996daede109dcf66001824d58712b
mohamed-aziz <medazizknani@gmail.com>
2015-01-13 17:27:33 +0000

    2nd commit

--- tictactoe_freepascal.pas
+++ tictactoe_freepascal.pas
@@ -8,11 +8,12 @@ type
   tab = array [1..9] of string;
   
 var
-  countPartie, i, scoreJoueur1, scoreJoueur2   : integer;
-  nomJoueur1, nomJoueur2    : string;
-  symJoueur1, symJoueur2, rep    : char;
-  bool   : boolean;
-  t                         : tab;
+  countPartie, scoreJoueur1, scoreJoueur2   : integer;
+  nomJoueur1, nomJoueur2                    : string;
+  symJoueur1, symJoueur2, rep               : char;
+  bool                                      : boolean;
+  t                                         : tab;
+  choix                                     : byte;
   
 procedure joueur(var nom : string; var sym, symJoueur1 : char; nbJoueur : byte) ;
 begin
@@ -31,13 +32,11 @@ begin
 end;
 
 
-procedure jeu(var t : tab; nomJoueur1, nomJoueur2 : string; symJoueur1, symJoueur2 : char;
-               var scoreJoueur1, scoreJoueur2 : integer);
+procedure jeu(t : tab; nomJoueur1, nomJoueur2 : string; symJoueur1, symJoueur2 : char;
+               var scoreJoueur1, scoreJoueur2 : integer; mode : byte);
 var
-  i, choix : integer;
-  e   : integer;
-  ega, st              : string;
-  a, tourJoueur1, tourJoueur2,cho                : boolean;
+  tour                        : integer;
+  a, tourJoueur1, tourJoueur2 : boolean;
 
 function compare(valeur1, valeur2, valeur3 : string; var st : string) : boolean;
 begin
@@ -56,11 +55,11 @@ end;
 
 procedure affichage(t : tab; scoreJoueur1, scoreJoueur2 : integer);
 var
-  i, x, y: byte;
+  i, x, y : byte;
 begin
   clrscr;
-  write('       ', symJoueur1, ' <> ', nomJoueur1, ' : ', scoreJoueur1, ' | ', 
-         symJoueur2, ' <> ', nomJoueur2, ' : ', scoreJoueur2);
+  write('       ', symJoueur1, ' % ', nomJoueur1, ' : ', scoreJoueur1, ' | ', 
+         symJoueur2, ' % ', nomJoueur2, ' : ', scoreJoueur2);
   writeln;
   writeln;
   writeln('                  ---|---|---');
@@ -108,7 +107,7 @@ end;
 procedure check(t : tab;var a: boolean;var scoreJoueur1, scoreJoueur2 : integer);
 var
   ega, st : string;
-  i   : integer;  
+  i       : integer;  
 begin
   if compare(t[1], t[2], t[3], st) or compare(t[4], t[5], t[6], st) or compare(t[7], t[8], t[9], st)
    or compare(t[1], t[4], t[7], st) or compare(t[2], t[5], t[8], st) or compare(t[3], t[6], t[9], st)
@@ -136,29 +135,57 @@ begin
        end;
 end;
 
+procedure saisieCase(var t : tab; nomJoueur : string; joueur : byte );
+var
+  choix : byte;
+  cho   : boolean;
+begin
+  repeat
+    cho := false;
+    if joueur <> 3 then
+      begin
+        textcolor(blue);
+        write(nomJoueur);
+        textcolor(Lightgray);
+        write(' entrer un nombre entre [1, 9] -> ');readln(choix);
+      end
+    else 
+      begin
+        randomize;
+        choix := random(9) + 1;
+      end;
+    if t[choix] = '' then
+      begin
+        if joueur = 1 then
+           t[choix] := '1'
+        else t[choix] := '0';
+        cho := true;
+      end;
+  until (choix in [1..9]) and cho;
+end;
+
 begin
   a := true;
+  randomize;
+  tour := random(2);
+  if tour = 1 then
+	begin
+	  tourJoueur1 := true;
+	  tourJoueur2 := false;
+	end
+  else
+	begin
+	  tourJoueur1 := false;
+	  tourJoueur2 := true;
+	end;
   while a do
     begin
-      tourJoueur1 := true;
-      tourJoueur2 := false;
       affichage(t, scoreJoueur1, scoreJoueur2);
       if tourJoueur1 then
         begin
-          repeat
-            cho := false;
-            textcolor(blue);
-            write(nomJoueur1);
-            textcolor(Lightgray);
-            write(' entrer un nombre entre [1, 9] -> ');readln(choix);
-            if t[choix] = '' then
-              begin
-                t[choix] := '1';
-                cho := true;
-              end;
-            tourJoueur1 := false;
-            tourJoueur2 := true;
-          until (choix in [1..9]) and cho;
+          saisieCase(t, nomJoueur1, 1);
+          tourJoueur1 := false;
+          tourJoueur2 := true;
         end;
       affichage(t, scoreJoueur1, scoreJoueur2);
       check(t, a, scoreJoueur1, scoreJoueur2);
@@ -166,39 +193,50 @@ begin
         begin
           if tourJoueur2 then
             begin
-              repeat
-              cho := false;
-            textcolor(red);
-            write(nomJoueur1);
-            textcolor(Lightgray);
-              write(' entrer un nombre entre [1, 9] -> ');readln(choix);
-              if t[choix] = '' then
-                begin
-                  t[choix] := '0';
-                  cho := true;
-                end;
-                tourJoueur2 := false;
-                tourJoueur1 := true;
-              until (choix in [1..9]) and cho;
+              if mode = 1 then
+                saisieCase(t, nomJoueur2, 0)
+              else saisieCase(t, nomJoueur2, 3);
+              tourJoueur1 := true;
+              tourJoueur2 := false;
             end;
         affichage(t, scoreJoueur1, scoreJoueur2);
         check(t, a, scoreJoueur1, scoreJoueur2);
     end;
-
    end;
 end;
 
 BEGIN
   repeat
+    clrscr;
+    writeln('          +---------------------------------------+');
+    writeln('          |           JEU de Tic Tac Toe          |');
+    writeln('          +---------------------------------------+');
+    writeln;
+    writeln;
+    writeln('          1) Joueur VS Joueur                      ');
+    writeln('          2) Joueur VS Machine (niveau facile)     ');
+    writeln;
+    writeln;
+    write  ('          Entrer un Choix > ');readln(choix);
+  until (choix = 1) or (choix = 2);
+  repeat
     countPartie := countPartie + 1;
     if countPartie = 1 then 
-      begin
-        joueur(nomJoueur1, symJoueur1, symJoueur1, 1);
-        joueur(nomJoueur2, symJoueur2, symJoueur1, 2);
-      end;
-    for i:=1 to 9 do
-      t[i] := '';
-    jeu(t, nomJoueur1, nomJoueur2, symJoueur1, symJoueur2, scoreJoueur1, scoreJoueur2);
+      if choix = 1 then
+        begin
+          joueur(nomJoueur1, symJoueur1, symJoueur1, 1);
+          joueur(nomJoueur2, symJoueur2, symJoueur1, 2);
+        end
+      else 
+        begin
+          joueur(nomJoueur1, symJoueur1, symJoueur1, 1);
+          nomJoueur2 := 'Machine';
+          if symJoueur1 = 'X' then 
+            symJoueur2 := 'O'
+          else
+            symJoueur2 := 'X';          
+        end;
+    jeu(t, nomJoueur1, nomJoueur2, symJoueur1, symJoueur2, scoreJoueur1, scoreJoueur2, choix);
     write('Une autre partie [Y/n] -> ');
     read (rep); 
     rep := upCase(rep);
@@ -213,4 +251,3 @@ BEGIN
   if scoreJoueur2 > scoreJoueur1 then
     writeln('Bien Joue ', nomJoueur2); 
 END.
-
