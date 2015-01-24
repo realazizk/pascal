afb7b3a70c31cc71a43c99589af23f947a32e4c0
mohamed-aziz <medazizknani@gmail.com>
2015-01-24 09:37:50 -0800

    added hard level

--- tictactoe_freepascal.pas
+++ tictactoe_freepascal.pas
@@ -1,6 +1,6 @@
 program tictactoe;
 {
-  Simple jeu realise par mohamed Aziz knani
+  Simple jeu realisee par mohamed Aziz knani
 }
 uses crt;
 
@@ -42,8 +42,10 @@ function compare(valeur1, valeur2, valeur3 : string; var st : string) : boolean;
 begin
   compare := false;
   (*writeln(valeur1, valeur2, valeur3); *)
+  
   if ((valeur1 <> '') and (valeur2 <> '')) and (valeur3 <> '') then
       if ((valeur1 = valeur2) and (valeur1 = valeur3)) and (valeur2 = valeur3) then
+        (* Moripon !!! *)
         begin
           compare := true;
           st := valeur1;
@@ -56,6 +58,18 @@ end;
 procedure affichage(t : tab; scoreJoueur1, scoreJoueur2 : integer);
 var
   i, x, y : byte;
+
+procedure inserer(x, y : byte; sym : char; jr : byte);
+begin
+  gotoxy(y, x);
+  if jr = 1 then 
+    textcolor(Lightblue)
+  else textcolor(red);
+  write(sym);
+  textcolor(Lightgray);
+  gotoxy(1, 11);
+end;
+
 begin
   clrscr;
   write('       ', symJoueur1, ' % ', nomJoueur1, ' : ', scoreJoueur1, ' | ', 
@@ -85,21 +99,9 @@ begin
             3, 6, 9 : y := 28;
           end;
         if t[i] = '1' then
-          begin
-            gotoxy(y, x);
-            textcolor(LightBlue);
-            write(symJoueur1);
-            textcolor(Lightgray);
-            gotoxy(1, 11);
-          end
+          inserer(x, y, symJoueur1, 1)
         else
-          begin
-            gotoxy(y, x);
-            textcolor(RED);
-            write(symJoueur2);
-            textcolor(Lightgray);
-            gotoxy(1, 11);
-          end; 
+          inserer(x, y, symJoueur2, 0)
       end;
     end;
 end;
@@ -128,21 +130,62 @@ begin
    ega := '';
    for i:=1 to 9 do
      ega := ega + t[i];
-     if (length(ega) = 9 ) and (st = '') then
-       begin
-         a := false;
-         writeln('Egalite !');
-       end;
+   if (length(ega) = 9 ) and (st = '') then
+     begin
+       a := false;
+       writeln('Egalite !');
+     end;
 end;
 
 procedure saisieCase(var t : tab; nomJoueur : string; joueur : byte );
 var
   choix : byte;
   cho   : boolean;
+
+function difficile(t : tab) : byte;
+var
+  st : byte;
+
+function com(c1, c2, c3 : string; n1, n2, n3 : byte; var foo : byte) : boolean;
+begin
+  if (c1 = c2) and (c3 = '') then 
+    begin 
+      foo := n3;
+      com := True;
+    end
+  else if (c1 = c3) and (c2 = '') then 
+    begin 
+      foo := n2;
+      com := True;
+    end
+  else if (c2 = c3) and (c1 = '') then 
+    begin
+      foo := n1;
+      com := True;
+    end
+  else 
+    com := False;
+end;
+
+begin
+  if com(t[1], t[2], t[3], 1, 2, 3, st) or com(t[4], t[5], t[6], 4, 5, 6, st) or com(t[7], t[8], t[9], 7, 8, 9, st)
+  or com(t[1], t[4], t[7], 1, 4, 7, st) or com(t[2], t[5], t[8], 2, 5, 8, st) or com(t[3], t[6], t[9], 3, 6, 9, st)
+  or com(t[1], t[5], t[9], 1, 5, 9, st) or com(t[3], t[5], t[7], 3, 5, 7, st) then
+     begin
+     difficile := st;
+     writeln(st); 
+     end
+  else 
+    begin
+      randomize;
+      difficile := random(9) + 1;
+    end;
+end;
+
 begin
   repeat
     cho := false;
-    if joueur <> 3 then
+    if not(joueur in [2..3]) then
       begin
         textcolor(blue);
         write(nomJoueur);
@@ -150,10 +193,17 @@ begin
         write(' entrer un nombre entre [1, 9] -> ');readln(choix);
       end
     else 
-      begin
-        randomize;
-        choix := random(9) + 1;
-      end;
+      if joueur = 2 then
+        begin
+          randomize;
+          choix := random(9) + 1;
+        end
+    else
+      if joueur = 3 then 
+    begin    
+    choix := difficile(t);
+
+  end;
     if t[choix] = '' then
       begin
         if joueur = 1 then
@@ -194,8 +244,8 @@ begin
           if tourJoueur2 then
             begin
               if mode = 1 then
-                saisieCase(t, nomJoueur2, 0)
-              else saisieCase(t, nomJoueur2, 3);
+                mode := 0;
+              saisieCase(t, nomJoueur2, mode);
               tourJoueur1 := true;
               tourJoueur2 := false;
             end;
@@ -208,17 +258,20 @@ end;
 BEGIN
   repeat
     clrscr;
+    textcolor(Lightblue);
     writeln('          +---------------------------------------+');
     writeln('          |           JEU de Tic Tac Toe          |');
     writeln('          +---------------------------------------+');
+    textcolor(Lightgray);
     writeln;
     writeln;
     writeln('          1) Joueur VS Joueur                      ');
     writeln('          2) Joueur VS Machine (niveau facile)     ');
+    writeln('          3) Joueur VS Machine (niveau difficile)  ');
     writeln;
     writeln;
     write  ('          Entrer un Choix > ');readln(choix);
-  until (choix = 1) or (choix = 2);
+  until choix in [1..3];
   repeat
     countPartie := countPartie + 1;
     if countPartie = 1 then 
